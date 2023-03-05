2148b367c0be82c46d6a83b36ee7cf13741b2c71
KasparK <kaspar_kytt@hotmail.com>
2023-03-05 20:59:14 +0200

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿Random rnd = new Random();
+
+int MyRandomNum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    MyRandomNum = rnd.Next(0, 11); //0-10
+    randomSum = randomSum + MyRandomNum;
+    Console.WriteLine($"My random number is: {MyRandomNum}");
+}
+
+Console.WriteLine($"random sum total: {randomSum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,12 @@
+﻿//FOR-LOOP
+int i = 0;
+for(i = 0; i < 5; i++) // i++ = i+1
+{
+    Console.WriteLine("We don't need no education.");
+}
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
